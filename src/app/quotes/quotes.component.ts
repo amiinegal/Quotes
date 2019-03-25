0c80c607d38f8515bfbac0e65d07b1bdb94200ef
amiinegal <you@example.com>
2019-03-25 11:37:17 +0300

    add property to each quote

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 import {Quote} from '../quote'
 
@@ -8,10 +9,10 @@ import {Quote} from '../quote'
 })
 export class QuotesComponent implements OnInit {
   quotes = [
-    new Quote(1,'You`re not to be so blind with patriotism that you can`t face reality','...Malcom x',new Date(2019,3,13 ),1,0),
-    new Quote(2,'The truth is, everyone is going to hurt you. You just got to find the ones worth suffering for.','...Bob marley',new Date(2019,5,17 ),12,9),
-    new Quote(3, 'imagination is more important than knowledge','...Abert Einstein',new Date(2019,4,14 ),2,9),
-    new Quote(3, 'Life consists of two days,one for you and one against you','...Ali Ibn Abu-Talib',new Date(2019,8,14 ),8,9),
+    new Quote(1,'You`re not to be so blind with patriotism that you can`t face reality','...Malcom x',new Date(2019,3,25 ),0,0),
+    new Quote(2,'The truth is, everyone is going to hurt you. You just got to find the ones worth suffering for.','...Bob marley',new Date(2019,3,25 ),0,0),
+    new Quote(3, 'imagination is more important than knowledge','...Abert Einstein',new Date(2019,3,25 ),0,0),
+    new Quote(3, 'Life consists of two days,one for you and one against you','...Ali Ibn Abu-Talib',new Date(2019,3,25 ),0,0),
    
 
 ]
@@ -46,4 +47,3 @@ toogleDetails(index){
   }
 
 }
-
