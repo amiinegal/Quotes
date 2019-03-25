6253d67513f43a223722a8ee3541bcff44d7b0b5
amiinegal <you@example.com>
2019-03-25 15:15:19 +0300

    fix the like and dislike button

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,7 @@
 export class Quote {
     public showDescription:boolean
-    constructor(public id:number, public name:string,public description:string,public completeDate:Date,public likeButtonClick:number,public dislikeButtonClick:number){        this.showDescription=false
+    constructor(public id:number, public name:string,public description:string,public completeDate:Date,public likes:number,){
+        this.showDescription=false
 
     }
 }
